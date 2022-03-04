d503313afdfa23721c168c9ebf3277d21c222f1d
rodtoral <rodtoral@LAB08-11.gregoriofer.local>
2022-03-04 19:17:48 +0100

    04/03/2022 Alejandro

---
+++ DamAle/src/main/java/Entornos.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author rodtoral
+ */
+public class Entornos {
+    public static void main(String[] args) {
+        System.out.println("Andres no se come ni una rosca ");
+    }
+}
